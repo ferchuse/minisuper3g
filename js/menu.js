c573bbfc0a8e3967a22093738015d3273c2bbc26
ferchuse <sistemas@glifo.mx>
2021-03-04 13:02:59 -0600

    Ajax cache false

--- js/menu.js
+++ js/menu.js
@@ -17,3 +17,4 @@ async function registerSW() {
   }
 }
 
+$.ajaxSetup({ cache: false });
--- scripts_carpetas.php
+++ scripts_carpetas.php
@@ -6,4 +6,4 @@
 <script src="../lib/common.js"></script>
 <script src="../lib/jquery.base64.js"></script>
 <script src="../lib/tableExport.js"></script>
-<script src="../js/menu.js"></script>
+<script src="../js/menu.js?v=<? date("Y-m-d")?>"></script>
