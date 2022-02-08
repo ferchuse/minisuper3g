5a542af949cc2ca4dbb79b71c7452a95620698d1
ferchuse <sistemas@glifo.mx>
2022-02-08 15:50:58 -0600

    Update resumen.php

--- corte/resumen.php
+++ corte/resumen.php
@@ -1,4 +1,7 @@
 <?php
+	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	header("Pragma: no-cache"); // HTTP 1.0.
+	header("Expires: 0"); // Proxies.
 	include("../login/login_success.php");
 	include("../funciones/generar_select.php");
 	include("../conexi.php");
